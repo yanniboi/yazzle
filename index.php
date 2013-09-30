95dba30353bbaf4f6740912b76aef6996818c63f
yanniboi <yan@yanniboi.com>
2013-09-30 18:04:44 +0100

    by yanniboi: Moved index.php to root directory and updated .htacces file.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . '/../vendor/autoload.php';
+include_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,16 +9,24 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 
 $request = Request::createFromGlobals();
 
-$routes = include __DIR__.'/../src/page_route.php';
+// Get path from browser;
+$path = include __DIR__.'/src/path.php';
+
+// Goto front if not set
+if (empty($path)) {
+  $path = 'hello';
+}
+
+$routes = include __DIR__.'/src/page_route.php';
 
 $context = new RequestContext();
 $context->fromRequest($request);
 $matcher = new UrlMatcher($routes, $context);
 
 try {
-  extract($matcher->match('/'.$request->query->get('path')), EXTR_SKIP);
+  extract($matcher->match('/'.$path), EXTR_SKIP);
   ob_start();
-  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+  include sprintf(__DIR__.'/src/pages/%s.php', $_route);
 
   $response = new Response(ob_get_clean());
 } catch (Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
